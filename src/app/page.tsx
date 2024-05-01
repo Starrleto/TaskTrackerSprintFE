a3a6a4aeaa1580b6555b86767a98b98f684b160a
Starrleto <ellierasuli@gmail.com>
2024-05-01 10:19:24 -0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { LoginComponent } from "@/Components/LoginComponent";
+import { Logincomponent } from "@/Components/LoginComponent";
 import NavbarComponent from "../Components/NavbarComponent";
 
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <NavbarComponent></NavbarComponent>
-      <LoginComponent></LoginComponent>
+      <Logincomponent></Logincomponent>
     </div>
   );
 }
